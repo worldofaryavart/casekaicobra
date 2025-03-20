93898630a630d34ca7d5b65a71e98160df3349f9
Shiva Saini <104831069+worldofaryavart@users.noreply.github.com>
2025-03-20 21:00:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['utfs.io'],
-    }
+  images: {
+    domains: ["utfs.io", "8rj0ncd67n.ufs.sh"],
+  },
 };
 
 export default nextConfig;
